8ee7224900ea485eeb3ba88045541d2b70f81f36
Samuel Escobar <sescobar8714@gmail.com>
2020-02-16 03:11:02 +0000

    Automatically backed up by Learn

--- lib/my_code.rb
+++ lib/my_code.rb
@@ -1,10 +1,10 @@
 # Your Code Here
-def map(array) 
+def map(array)
 new_array = []
 i = 0
-  while i < array.count 
+  while i < array.count
     new_array(yield(array[i]))
-  i += 1 
-  end 
+  i += 1
+  end
 
 end 
